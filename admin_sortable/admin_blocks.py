2b493aa68f4ada238ca7755087796609021f7b01
Andy Baker <andy@andybak.net>
2014-02-03 12:07:55 +0000

    use new way of loading admin js

--- admin_sortable/admin_blocks.py
+++ admin_sortable/admin_blocks.py
@@ -4,7 +4,7 @@ import django_admin_blocks
 
 django_admin_blocks.register({
     'script_blocks': [
-        '<script src="%sjs/admin_sortable/jquery-ui-1.10.3.custom.min.js"></script>' % settings.STATIC_URL,
-        '<script src="%sjs/admin_sortable/admin_sortable.js"></script>' % settings.STATIC_URL,
+        {'url_path': 'js/admin_sortable/jquery-ui-1.10.3.custom.min.js'},
+        {'url_path': 'js/admin_sortable/admin_sortable.js'},
     ],
-    })
+})
