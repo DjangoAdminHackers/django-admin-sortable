d941f1d9a198a8291e1ac99bd28b8100acebaf9d
fruitschen <fruitschen@gmail.com>
2015-03-26 20:37:16 +0800

    sortable stacked inlines

--- admin_sortable/static/js/admin_sortable/admin_sortable.js
+++ admin_sortable/static/js/admin_sortable/admin_sortable.js
@@ -60,11 +60,59 @@ function sortable_inlines() {
     })
 }
 
+
+function sortable_stacked_inlines() {
+    var $target_inlines = $('div.page-inlines').filter(
+        function(index) {//only convert the inlines that has an "order" field.
+            return $('div.field-order', this).length > 0;
+        })
+    $target_inlines.sortable({
+        items: 'div.inline-related',
+        handle: 'h3',
+        helper: 'clone',
+        update: function() {
+            // Renumber all hidden 'order' fields to reflect new ordering
+            $(this).find('div.inline-related').each(function(i) {
+                // TODO This doesn't allow ordering of unsaved inlines
+                // Need to include all rows
+                // And only add 'order' val to non-empty ones.
+                //if ($(this).find('input[class$=vIntegerField]').val()) {
+                    $(this).find('input[id$=field-order]').val(i+1);
+                    $(this).find('input[id$=order]').val(i+1);
+                //}
+            });
+        },
+        // fix for #33, if you re-order by drag and drop all text is lost
+        // see also http://tinymce.moxiecode.com/punbb/viewtopic.php?id=17074
+        start: sortable_start_hook,
+        stop: sortable_stop_hook
+    });
+    // Change the cursor
+    $target_inlines.find('h3')
+        .css('cursor', 'move')
+    $target_inlines.each(function(index){
+        //hide 'Order' and hide it field.  
+        $(this).find('div.field-order').hide();
+        //Add a gripper on the left edge to indicate 'dragability' TODO this needs improving visually
+        $(this).find('div.inline-related h3:first-child')
+            .css('padding-left', '25px')
+            /* TODO Don't hardcode static dir */
+            .css('background-image', 'url(/static/images/gripper.png)')
+            .css('background-repeat','no-repeat')
+            .css('background-position','left center')
+            .attr('title', 'Drag up and down to re-order these items');
+        $(this).find('div:first-child p').css('padding-left','30px');
+    })
+}
+
 $(document).ready(function () {
     if ($('body').hasClass('change-form')) {
         // If there's an inline with an 'order' column then make it sortable
         if ($('div.tabular.inline-related .module table input[id$=order]').length) {
             sortable_inlines();
+        }
+        if ($('div.inline-related input[id$=order]').length) {
+            sortable_stacked_inlines();
         }
     }
 });
